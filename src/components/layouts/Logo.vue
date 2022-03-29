91f62dcfcd61483776d807f54d487c74f8838a8a
Akbar Hidayatuloh <01akbarh@gmail.com>
2022-03-29 13:39:48 +0700

    format volar navbar

--- src/components/layouts/Logo.vue
+++ src/components/layouts/Logo.vue
@@ -1,10 +1,6 @@
 <template>
   <router-link to="/" class="flex items-center">
-    <img
-      src="@/assets/img/logo.svg"
-      class="h-8 ml-3 sm:ml-0 sm:h-8"
-      alt="Flowbite Logo"
-    />
+    <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
   </router-link>
 </template>
 
